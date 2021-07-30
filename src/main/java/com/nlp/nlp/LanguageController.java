bd90596e4b71b2d377afa1434dee7c58d69f354f
ronaldogjini <ronaldogjini@users.noreply.github.com>
2021-07-30 23:35:45 +0200

    CORS

--- src/main/java/com/nlp/nlp/LanguageController.java
+++ src/main/java/com/nlp/nlp/LanguageController.java
@@ -16,18 +16,18 @@ public class LanguageController {
     @PostMapping("/language")
     public Text calculateLanguage(@RequestBody String text) {
         String languageFound = NLPMain.findLanguage(text);
-        if (languageFound == "en") {
+        if (languageFound.equals("en")) {
             return new Text("English");
-        } else if (languageFound == "de") {
+        } else if (languageFound.equals("de")) {
             return new Text("German");
-        } else if (languageFound == "fr") {
+        } else if (languageFound.equals("fr")) {
             return new Text("French");
-        } else if (languageFound == "it") {
+        } else if (languageFound.equals("it")) {
             return new Text("Italian");
-        } else if (languageFound == "Ru") {
+        } else if (languageFound.equals("ru")) {
             return new Text("Russian");
         }
-        return new Text("Write more");
+        return new Text(languageFound);
 
     }
 }
