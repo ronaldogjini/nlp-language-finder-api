91cc9e62ea5e957fe37841e4311e58010e60d004
ronaldogjini <ronaldogjini@users.noreply.github.com>
2021-07-30 20:31:35 +0200

    CORS

--- src/main/java/com/nlp/nlp/LanguageController.java
+++ src/main/java/com/nlp/nlp/LanguageController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class LanguageController {
 
-
+    @CrossOrigin
     @GetMapping("/languages")
     public Language calculateLanguages() {
         return new Language("hellooooo");
     }
 
-    @CrossOrigin(origins = "http://localhost:8000")
+    @CrossOrigin
     @PostMapping("/language")
     public Text calculateLanguage(@RequestBody String text) {
         String languageFound = NLPMain.findLanguage(text);
